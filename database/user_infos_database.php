1292d461fbbfdd9eb34d68c2b77e03877625317d
SacleuxBenoit <benoitsacleux@outlook.fr>
2021-01-20 19:17:32 +0100

    add a verification for see if an input is empty or not :arrow_right: user_infos_database.php

--- database/user_infos_database.php
+++ database/user_infos_database.php
@@ -1,8 +1,11 @@
 <?php
 include('connection_database.php');
 
-$addUser = $bdd->prepare('INSERT INTO player_infos(firstPlayer,secondPlayer) VALUES(:firstPlayer, :secondPlayer)');
-$addUser->bindParam(':firstPlayer', $_POST['firstPlayer']);
-$addUser->bindParam(':secondPlayer', $_POST['secondPlayer']);
-$addUser->execute();
+if(!empty($_POST['firstPlayer']) && !empty($_POST['secondPlayer'])){
+    $addUser = $bdd->prepare('INSERT INTO player_infos(firstPlayer,secondPlayer) VALUES(:firstPlayer, :secondPlayer)');
+    $addUser->bindParam(':firstPlayer', $_POST['firstPlayer']);
+    $addUser->bindParam(':secondPlayer', $_POST['secondPlayer']);
+    $addUser->execute();
+}
+
 ?>
